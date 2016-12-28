89e4ea9ad623282e81b8bcaa84f98a9966c624cc
Christian Nastasi <christian.nastasi@gmail.com>
2016-12-28 01:07:45 +0100

    100% coverage

---
+++ tests/Helper/TimeDefaultTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: christian
+ * Date: 12/28/16
+ * Time: 12:44 AM
+ */
+
+namespace Helper;
+
+
+use AsyncHttpClient\Helper\TimeDefault;
+
+class TimeDefaultTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $time = new TimeDefault();
+
+        $now = $time->now();
+
+        \PHPUnit_Framework_Assert::assertEquals('double', gettype($now));
+        \PHPUnit_Framework_Assert::assertEquals(microtime(true), $now, 'Now is not now', 0.01);
+    }
+}
---
+++ tests/Service/AsyncHttpGenericServiceTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: christian
+ * Date: 12/28/16
+ * Time: 12:52 AM
+ */
+
+namespace Service;
+
+use AsyncHttpClient\Service\AsyncHttpGenericService;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use PHPUnit_Framework_Assert as PHPUnit;
+use React\HttpClient\Response;
+
+class AsyncHttpGenericServiceTest extends MockeryTestCase
+{
+    public function test()
+    {
+        $method   = 'GET';
+        $url      = 'http://www.google.it';
+        $called   = false;
+        $data     = '{ "result": true }';
+        $response = $this->mockResponse();
+
+        $callback = function ($d, Response $r) use (&$called, $data, $response) {
+            $called = true;
+
+            PHPUnit::assertEquals($data, $d);
+            PHPUnit::assertEquals($response, $r);
+        };
+
+        $service = new AsyncHttpGenericService($method, $url, $callback);
+
+        $service->execute($data, $response);
+
+        PHPUnit::assertTrue($called);
+        PHPUnit::assertEquals($method, $service->getMethod());
+        PHPUnit::assertEquals($url, $service->getUrl());
+    }
+
+    private function mockResponse()
+    {
+        $mock = \Mockery::mock(Response::class);
+
+        return $mock;
+    }
+}
